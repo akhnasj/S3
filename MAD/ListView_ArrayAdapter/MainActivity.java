de789005fe8b2f51434700f8f7538ce969f8d788
Akhna <akhnasj1612@gmail.com>
2024-11-21 19:15:22 +0530

    ListView ArrayAdapter

---
+++ MAD/ListView_ArrayAdapter/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.listview_arrayadapter;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.sql.Array;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView list;
+    String[] arr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        list = (ListView) findViewById(R.id.listView);
+        arr = new String[]{"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"};
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arr);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Item " +arr[position]+ " has been clicked!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
